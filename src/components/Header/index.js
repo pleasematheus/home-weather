eab8200b3d3348200e452f76688784fdcd19cf12
pleasematheus <matheus4k@gmail.com>
2021-03-19 01:20:15 -0400

    Header and conditions

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,47 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
+
+import { Ionicons } from '@expo/vector-icons'
 
 export default function Header() {
 	return (
-		<View>
-			<Text>
-				Header
-			</Text>
-		</View>
+		<LinearGradient
+			style={ styles.header }
+			colors={[ '#1ED6FF', '#97C1FF' ]}
+		>
+			<Text style={ styles.date }>20/07/2020</Text>
+			<Text style={ styles.city }>UBERLANDIA</Text>
+			<Ionicons
+				name="cloud"
+				color="#FFF"
+				size={ 150 }
+			/>
+			<Text style={ styles.temp }>30°</Text>
+		</LinearGradient>
 	)
 }
+
+const styles = StyleSheet.create({
+	header: {
+		width: '90%',
+		height: '55%',
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderRadius: 8
+	},
+	date: {
+		color: '#FFF',
+		fontSize: 17
+	},
+	city: {
+		color: '#FFF',
+		fontSize: 20,
+		fontWeight: 'bold'
+	},
+	temp: {
+		color: '#FFF',
+		fontSize: 80,
+		fontWeight: 'bold'
+	}
+})
